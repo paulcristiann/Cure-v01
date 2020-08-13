38effeaaa9e0e5a05cb960687f3d7c9f19a1c84a
Paul-Cristian Vasile <paul.cristian.vasile04@gmail.com>
2020-08-13 09:16:44 +0300

    Updated-1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import asyncio
 import configparser
 import datetime
-import sys
-import traceback
 import re
 import sqlite3
+import sys
 import time
+import traceback
 from threading import Thread
 
 import requests
@@ -19,8 +19,8 @@ try:
     api_hash = config['USER']['ApiHash']
     listening_channel = config['USER']['ListenOn']
     webhook_list = config['USER']['Webhooks']
-    #Wait time in seconds
-    wait_time = 10
+    # Wait time in seconds
+    wait_time = 60
     ################################################
 except Exception:
     traceback.print_exc()
@@ -29,18 +29,19 @@ except Exception:
 def print_status():
     print('---------------------------------------')
     print('CURE-v01 started @', datetime.datetime.now())
-    print('Listens: ', listening_channel)
-    print('Webhook: ', webhook_list)
+    print('Listens:', listening_channel)
+    print('Webhook:', webhook_list)
+    print('Waiting time:', datetime.timedelta(seconds=wait_time))
     print('---------------------------------------')
 
 
-async def send_to_webhook(payload, webhook):
+def send_to_webhook(payload, webhook):
     print('Sending payload', payload, 'to webhook', webhook)
     try:
         r = requests.post(webhook, data=payload)
-        print('Message sent')
     except:
         print('Error sending data to the webhook')
+
 
 def start_thread(coin, time_to_wait):
     con = sqlite3.connect('cure_db.sqlite')
@@ -48,17 +49,38 @@ def start_thread(coin, time_to_wait):
     del_sql = f"DELETE from pending where coin = \"{coin}\""
     cs.execute(del_sql)
     con.commit()
-    print(f'Started worker for coin {coin}')
-    sql = ''' INSERT INTO pending(coin,begin_date,thread_id)
+    print(
+        f'Started worker for coin {coin} - waiting until {datetime.datetime.now() + datetime.timedelta(seconds=wait_time)}')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    sql = ''' INSERT INTO pending(coin,begin_date,thread_status)
                   VALUES(?,?,?) '''
-    pend = (coin, str(datetime.datetime.now()), str(coin+'-thread'))
+    pend = (coin, str(datetime.datetime.now()), 'running')
     cs.execute(sql, pend)
     con.commit()
-    #Now wait
-    time.sleep(time_to_wait)
-    print('Time Finished')
-    cs.execute(del_sql)
-    con.commit()
+    finishing_at = datetime.datetime.now() + datetime.timedelta(seconds=wait_time)
+    while 1:
+        data = cs.execute(f'select * from pending where coin=\"{coin}\";').fetchall()[0]
+        if finishing_at <= datetime.datetime.now():
+            print()
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            print(f'{coin}-Thread finished')
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            cs.execute(del_sql)
+            con.commit()
+            # send to webhook
+            payload = f'symbol={coin}BTC,type=cancelorders;symbol={coin}BTC,type=market,side=sell,quantity=100;'
+            send_to_webhook(payload, webhook_list)
+            return
+        else:
+            if data[3] == 'stopped':
+                print()
+                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                print(f'{coin}-Thread was stopped')
+                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                cs.execute(del_sql)
+                con.commit()
+                return
+
 
 async def parser(message):
     print('')
@@ -69,23 +91,52 @@ async def parser(message):
 
     for eachline in lines:
         if ('SIGNAL BUY' in eachline.upper()):
-            #Signal buy logic
+            # Signal buy logic
             coin = re.findall(r"#(\w+)", eachline)[0]
-            main = Thread(target=start_thread, args=[coin,wait_time], name=coin+'-thread')
-            main.start()
+            thread = Thread(target=start_thread, args=[coin, wait_time], name=coin + '-thread')
+            thread.start()
             return
-        if('BUY AGAIN' in eachline.upper()):
-            #Buy again logic
+        if ('BUY AGAIN' in eachline.upper()):
+            # Buy again logic
             coin = re.findall(r"#(\w+)", eachline)[0]
+            print(f'Buy again received for {coin}')
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            update_sql = f'''UPDATE pending SET thread_status = 'stopped' WHERE coin = \"{coin}\";'''
+            con = sqlite3.connect('cure_db.sqlite')
+            crs = con.cursor()
+            crs.execute(update_sql)
+            con.commit()
+            crs.close()
+            con.close()
+            time.sleep(2)
+            new_time = wait_time / 2
+            thread = Thread(target=start_thread, args=[coin, new_time], name=coin + '-thread')
+            thread.start()
+            return
+        if ('REACHED' in eachline.upper()):
+            # Target reached logic
+            coin = re.findall(r"#(\w+)", eachline)[0]
+            print(f'Target reached received for {coin}')
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            update_sql = f'''UPDATE pending SET thread_status = 'stopped' WHERE coin = \"{coin}\";'''
+            con = sqlite3.connect('cure_db.sqlite')
+            crs = con.cursor()
+            crs.execute(update_sql)
+            con.commit()
+            crs.close()
+            con.close()
+            return
 
-            return
+    print('Sintax not found')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
     return
 
-async def Run():
 
+async def Run():
     try:
 
-        #Cache telegram entities
+        # Cache telegram entities
         dialogs = await client.get_dialogs()
 
         @client.on(events.NewMessage(chats=listening_channel))
@@ -108,7 +159,7 @@ sql_create_table = """ CREATE TABLE IF NOT EXISTS pending (
                                         id integer PRIMARY KEY,
                                         coin text NOT NULL,
                                         begin_date text,
-                                        thread_id text
+                                        thread_status text
                                     ); """
 c.execute(sql_create_table)
 
@@ -118,15 +169,16 @@ recovered_list = cursor.fetchall()
 
 for entry in recovered_list:
     finishing_time = datetime.datetime.strptime(entry[2], '%Y-%m-%d %H:%M:%S.%f') + datetime.timedelta(seconds=wait_time)
-    if(datetime.datetime.now() > finishing_time):
+    if (datetime.datetime.now() > finishing_time):
         c.execute(f"DELETE from pending where coin = \"{entry[1]}\"")
         conn.commit()
     else:
-        #Reopen threads with remaining time
+        # Reopen threads with remaining time
         seconds_remaining = (finishing_time - datetime.datetime.now()).seconds
         main = Thread(target=start_thread, args=[entry[1], seconds_remaining], name=entry[1] + '-thread')
         main.start()
-        print("Recovered " + str(entry[1]) + " thread with " + str(finishing_time - datetime.datetime.now()) + " time remaining")
+        print("Recovered " + str(entry[1]) + " thread with " + str(
+            finishing_time - datetime.datetime.now()) + " time remaining")
 
 c.close()
 conn.close()
